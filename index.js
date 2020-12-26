13a2a5c647e88af5a2b65657f113e38322734b0c
pingvy <betttbettt@yandex.ru>
2020-12-26 20:53:18 +0600

    project to check

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
 let main = document.querySelector('.main');
 
-//profile block:
+//profile section
 let profile = main.querySelector('.profile');
-let profileContainerText = profile.querySelector('.profile__container-text');
-let profileTitle = profileContainerText.querySelector('.profile__title');
-let profileDescription = profileContainerText.querySelector('.profile__subtitle');
-let profileEditButton = profileContainerText.querySelector('.profile__edit-button');
+let containerText = profile.querySelector('.profile__container-text');
+let profileTitle = containerText.querySelector('.profile__title');
+let profileSubtitle = containerText.querySelector('.profile__subtitle');
+let profileEditButton = containerText.querySelector('.profile__edit-button');
 
-//popup block:
+//overlay & popup section
 let overlay = document.querySelector('.overlay');
 let popup = overlay.querySelector('.popup');
-let popupExitButton = popup.querySelector('.popup__exit-button');
-let submitButton = popup.querySelector('.popup__submit');
 let inputName = popup.querySelector('.popup__input-name');
 let inputDescription = popup.querySelector('.popup__input-description');
+let submitButton = popup.querySelector('.popup__submit-button');
+let closeButton = popup.querySelector('.popup__close-button');
 
-//popup placeholder's names: 
-inputName.placeholder = profileTitle.textContent;
-inputDescription.placeholder = profileDescription.textContent;
-
-//show or hide popup:
+//popup appearance function
 function popupVisibility() {
-  overlay.classList.toggle('overlay_hidden');
+  overlay.classList.toggle('overlay_active');
 }
 
-//popup handlers:
+//popup appearance handlers
 profileEditButton.addEventListener('click', popupVisibility);
+closeButton.addEventListener('click', popupVisibility);
 overlay.addEventListener('click', (event) => {
   if (event.target === event.currentTarget) {
     popupVisibility();
   }
 });
 
-//popup submit:
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
+//profile edit function
+function profileAction() {
   profileTitle.textContent = inputName.value;
-  profileDescription.textContent = inputDescription.value;
+  profileSubtitle.textContent = inputDescription.value;
   popupVisibility();
+}
+
+//profile eddit handler
+popup.addEventListener('submit', (event) => {
+  event.preventDefault();
+  profileAction()
 });
+
+//placeholders for inputs
+inputName.placeholder = profileTitle.textContent;
+inputDescription.placeholder = profileSubtitle.textContent;
